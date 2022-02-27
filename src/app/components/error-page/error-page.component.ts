b12a6c8e3b7d0579f1df2f7dc2c7d08c6d45c1ac
Alfian Rikzandi <denydirgantara94@gmail.com>
2022-02-27 11:27:32 +0700

    fix: error page

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BaseErrorQueryParam } from 'src/app/shared/types';
 
 @Component({
   selector: 'app-error-page',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorPageComponent implements OnInit {
 
-  constructor() { }
+  title!: string;
+  desc!: string;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.initQueryParam();
+  }
+
+  initQueryParam(): void {
+    const queryParamInit: BaseErrorQueryParam = this.activatedRoute.snapshot.queryParams as BaseErrorQueryParam;
+
+    if (queryParamInit.title) {
+      this.title = queryParamInit.title;
+    } else {
+      this.title = 'Something went wrong';
+    }
+
+    if (queryParamInit.desc) {
+      this.desc = queryParamInit.desc;
+    } else {
+      this.desc = 'We&apos;re sorry. We have a problem while showing page, try again leter.';
+    }
+  }
 
   ngOnInit(): void {
   }
