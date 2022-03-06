d5698f88025d7da9092b3bd966e7e0aca61ed4b7
Alfian Rikzandi <denydirgantara94@gmail.com>
2022-03-06 11:02:42 +0700

    chore: fix env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
   RECAPTCHA_V3_SITE_KEY: '6LfAjaAeAAAAACPr7rpb3nQtLJP5uXHk6TzFKjfn',
 
   // Cookies
-  COOKIE_SID: 'sid-myponyasia',
-  COOKIE_SIDR: 'sidr-myponyasia',
+  COOKIE_SID: 'SID-MYPONYASIA',
+  COOKIE_SIDR: 'SIDR-MYPONYASIA',
   COOKIE_DOMAIN: '.myponyasia.com',
   COOKIE_SECURE: true,
   COOKIE_HTTP_ONLY: true,
